745f64a5d70524b285d885dd90dab64534a75b3d
LuisMesa <la.mesa10@uniandes.edu.co>
2017-10-27 16:20:53 -0500

    First commit, Map and Markers working

---
+++ src/Map.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker';
+
+import './Map.css';
+
+class Map extends Component {
+  handleClick(event){
+    console.log('lat',event.lat);
+    console.log('lng',event.lng);
+  }
+  render() {
+    return (
+      <div className="Map">
+          <GoogleMapReact
+              options={{styles: [{
+                "featureType": "all",
+                "elementType": "labels",
+                "stylers": [{
+                  "visibility": "#on"
+                }]
+              }]}}
+              center={{lat: 4.699494, lng: -74.055148}}
+              zoom={15}
+              onClick={(event)=>this.handleClick(event)}
+          >
+            <Marker tooltip='Hola' lat='4.7010032' lng='-74.055497'/>
+            
+          </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default Map;
